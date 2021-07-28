3f8362a5b7f2f8b026a4be7d58dcc99f7b4f2bd4
unknown <marcojl2000@hotmail.com>
2021-07-28 18:53:41 +0200

    and another one

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const pokemonimages = ["Absol/stat-absol.png", "Amphinobi/stat-greninja.png", "Blancoton/stat-eldegoss.png","Carchacrok/stat-garchomp.png","Crabaraque/stat-crustle.png","Dracaufeu/stat-charizard.png", "Ectoplasma/stat-gengar.png","Feunard Alola/stat-alolan-ninetales.png","Flagadoss/stat-slowbro.png","Flambusard/stat-talonflame.png","Florizarre/stat-venusaur.png","Grodoudou/stat-wigglytuff.png","Lucario/stat-lucario.png","Mackogneur/stat-machamp.png","Mr Mime/stat-mrmime.png","Nigosier/stat-cramorant.png","Pikachu/stat-pikachu.png","Pyrobut/stat-cinderace.png","Ronflex/stat-snorlax.png","Zeraora/stat-zeraora.png"];
 
 var randomPokemon = pokemonimages.splice(Math.floor(Math.random() * pokemonimages.length),1);
-document.querySelectorAll('.bg-pokemon')[0].src = randomPokemon;
+document.querySelectorAll('.bg-pokemon')[0].src = "assets/" + randomPokemon;
 
 
 var andAnotherOne = pokemonimages[Math.floor(Math.random() * pokemonimages.length)];
 
-document.querySelectorAll('.bg-pokemon')[1].src = andAnotherOne;
+document.querySelectorAll('.bg-pokemon')[1].src = "assets/" + andAnotherOne;
